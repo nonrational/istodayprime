d6048579b0383244fd3bc2142116e86cd2d3b34a
Alan Norton <me@alannorton.com>
2013-11-30 18:45:50 -0500

    add templating with ejs+css

--- web.js
+++ web.js
@@ -1,9 +1,27 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
 
-app.get('/', function(request, response) {
-  response.send('is today a prime day?');
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Is Today a Prime day?', result: "No." });
 });
 
 var port = process.env.PORT || 5000;
