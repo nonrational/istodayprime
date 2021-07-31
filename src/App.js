2a810dae5a808f0d6fda3b1bb3f3a6dfd8cda82c
Alan Norton <git@nonration.al>
2021-07-31 13:19:51 -0400

    attempt 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg'
 import './App.css'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom'
 
-const Primality = (a) => {
-  debugger
-  return <>Not Sure!</>
+const timeInZone = (timeZone) => {
+  const formatter = new Intl.DateTimeFormat([], {
+    timeZone: timeZone,
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  })
+
+  return formatter.format(new Date())
 }
 
-const GeoLocate = () => {}
+const Primality = ({ timeZone }) => {
+  const overall = true
+  return <p class='overall'>{overall ? 'Yup.' : 'Nope.'}%></p>
+}
+
+const GeoRedirect = ({ timeZone }) => {
+  useHistory().push(`/${timeZone}`)
+  return null
+}
 
 function App() {
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+
   return (
     <div className='App'>
       <Router>
         <Switch>
-          <Route path='/:first/:second'>
-            <Primality />
+          <Route path='/:region/:zone'>
+            <Primality timeZone={timeZone} />
           </Route>
           <Route path='/'>
-            <GeoLocate />
+            <GeoRedirect timeZone={timeZone} />
           </Route>
         </Switch>
       </Router>
